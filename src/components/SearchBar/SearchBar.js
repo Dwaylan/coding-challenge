20c21b6651f1fb38018c633c7b6e4a11c010d8b3
Dwaylan <Dwaylan.Applewhite@gmail.com>
2021-06-30 12:43:40 -0400

    first commit

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import axios from "axios";
+import "../SearchBar/SearchBar.css";
 
 class SearchBar extends Component {
   constructor(props) {
@@ -9,17 +11,55 @@ class SearchBar extends Component {
       loading: false,
       message: "",
     };
+    this.cancel = "";
   }
+
+  fetchSearchResults = (updatedPageNo, query) => {
+    // const pageNumber = updatedPageNo ? `&page=4${updatedPageNo}` : "";
+    const searchUrl = `https://challenge-rest-api.vercel.app/business${query}`;
+
+    if (this.cancel) {
+      this.cancel.cancel();
+    }
+    this.cancel = axios.CancelToken.source();
+    axios
+      .get(searchUrl, {
+        cancelToken: this.cancel.token,
+      })
+      .then((res) => {
+        // const resultNotFoundMsg = 
+        console.log(res.data);
+      })
+      .catch(error =>{
+        if(axios.isCancel(error) || error){
+          this.setState({
+            loading: false,
+            message: 'Failed to fetch data'
+          })
+        }
+      });
+  };
+
+  handleOnInputChange = (event) => {
+    const query = event.target.value;
+    this.setState({ query: query, loading: true, message: "" }, () => {
+      this.fetchSearchResults(query);
+    });
+  };
   render() {
+    const { query } = this.state;
+    console.log(this.state);
     return (
       <div className="container">
         <h2 className="heading">Business table challenge</h2>
-        <label className="search label" htmlFor="search-input">
+        <label className="search-label" htmlFor="search-input">
           <input
             type="text"
+            name="query"
             placeholder="type here"
-            value=""
+            value={query}
             id="search-input"
+            onChange={this.handleOnInputChange}
           />
         </label>
       </div>
